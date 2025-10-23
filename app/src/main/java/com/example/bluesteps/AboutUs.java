9d0ab082c3a5029dd565ad51c6b0b0bb173c5d06
Kaan Kaya <kikitwin17@gmail.com>
2025-10-24 00:05:47 +0300

    started to quiz game and nav bar interaction

--- app/src/main/java/com/example/bluesteps/AboutUs.java
+++ app/src/main/java/com/example/bluesteps/AboutUs.java
@@ -1,6 +1,7 @@
 package com.example.bluesteps;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,12 @@ public class AboutUs extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void navHomePage(View view){
+        Nav.goToHomePage(view);
+    }
+    public void goToEducation(View view){
+        Nav.goToEducation(view);
     }
 }
--- app/src/main/java/com/example/bluesteps/GameQuiz.java
+++ app/src/main/java/com/example/bluesteps/GameQuiz.java
@@ -1,6 +1,7 @@
 package com.example.bluesteps;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,25 @@ public class GameQuiz extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void quizGameAnswer1(View view){
+
+    }
+    public void quizGameAnswer2(View view){
+
+    }
+    public void quizGameAnswer3(View view){
+
+    }
+    public void quizGameAnswer4(View view){
+
+    }
+
+    public void navHomePage(View view){
+        Nav.goToHomePage(view);
+    }
+    public void goToEducation(View view){
+        Nav.goToEducation(view);
     }
 }
